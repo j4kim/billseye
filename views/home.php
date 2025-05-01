ec32e371fb668c6771f9565151c10dac72e98eef
Joaquim Perez <jivkim@gmail.com>
2025-05-01 14:41:55 +0200

    reusable layout

--- views/home.php
+++ views/home.php
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bill's Eye</title>
-</head>
-
-<body>
-    <h1>Bill's Eye</h1>
-</body>
-
-</html>
+<?php $this->layout('layout');
---
+++ views/layout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bill's Eye</title>
+</head>
+
+<body>
+    <h1>Bill's Eye</h1>
+
+    <?= $this->section('content') ?>
+</body>
+
+</html>
