a9fda40d865fc201395765701f0cef3820cc36e9
Joaquim Perez <jivkim@gmail.com>
2025-05-06 00:36:15 +0200

    add QrBillGenerator from old branch

---
+++ app/Tools/QrBillGenerator.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Tools;
+
+use Sprain\SwissQrBill as QrBill;
+use Sprain\SwissQrBill\PaymentPart\Output\HtmlOutput\HtmlOutput;
+
+class QrBillGenerator
+{
+    public static function generate(array $data)
+    {
+        $qrBill = QrBill\QrBill::create();
+
+        $qrBill->setCreditor(
+            QrBill\DataGroup\Element\StructuredAddress::createWithStreet(
+                ...$data['creditor']
+            )
+        );
+
+        $qrBill->setCreditorInformation(
+            QrBill\DataGroup\Element\CreditorInformation::create(
+                $data['iban']
+            )
+        );
+
+        if ($data['debtor']['name']) {
+            $qrBill->setUltimateDebtor(
+                QrBill\DataGroup\Element\StructuredAddress::createWithStreet(
+                    ...$data['debtor']
+                )
+            );
+        }
+
+        $qrBill->setPaymentAmountInformation(
+            QrBill\DataGroup\Element\PaymentAmountInformation::create(
+                $data['currency'],
+                $data['amount']
+            )
+        );
+
+        if ($data['reference']) {
+            $qrBill->setPaymentReference(
+                QrBill\DataGroup\Element\PaymentReference::create(
+                    QrBill\DataGroup\Element\PaymentReference::TYPE_SCOR,
+                    QrBill\Reference\RfCreditorReferenceGenerator::generate($data['reference'])
+                )
+            );
+        } else {
+            $qrBill->setPaymentReference(
+                QrBill\DataGroup\Element\PaymentReference::create(
+                    QrBill\DataGroup\Element\PaymentReference::TYPE_NON
+                )
+            );
+        }
+
+        if ($data['additional-information']) {
+            $qrBill->setAdditionalInformation(
+                QrBill\DataGroup\Element\AdditionalInformation::create(
+                    $data['additional-information']
+                )
+            );
+        }
+
+        $output = new HtmlOutput($qrBill, 'fr');
+
+        try {
+            $html = $output->getPaymentPart();
+        } catch (\Exception) {
+            foreach ($qrBill->getViolations() as $violation) {
+                print $violation->getMessage() . "\n";
+            }
+            exit;
+        }
+
+        return $html;
+    }
+}
