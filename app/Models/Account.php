820fd0120e50a1a2dd3b8c8d7a00b7f93af9f23c
Joaquim Perez <jivkim@gmail.com>
2025-05-07 00:30:14 +0200

    create Account model and migration

---
+++ app/Models/Account.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Orchid\Screen\AsSource;
+
+class Account extends Model
+{
+    use AsSource;
+}
---
+++ database/migrations/2025_05_06_161700_create_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('email')->nullable();
+            $table->json('smtp_config')->nullable();
+            $table->string('iban');
+            $table->string('name');
+            $table->string('street');
+            $table->string('building_number')->nullable();
+            $table->integer('postal_code');
+            $table->string('city');
+            $table->string('country');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
