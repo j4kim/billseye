c37537d1f09ef681ed97bf87e06f09f7df7cea19
Joaquim Perez <jivkim@gmail.com>
2025-05-07 01:28:21 +0200

    encrypt account smtp config

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -9,4 +9,11 @@ use Orchid\Screen\AsSource;
 class Account extends Model
 {
     use AsSource, Filterable;
+
+    protected function casts(): array
+    {
+        return [
+            'smtp_config' => 'encrypted:array',
+        ];
+    }
 }
