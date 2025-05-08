73b7213eac0dd65d6ffab73ba25caf7e4f359d38
Joaquim Perez <jivkim@gmail.com>
2025-05-08 17:23:30 +0200

    prepare invoice layout

--- resources/views/invoice/preview.blade.php
+++ resources/views/invoice/preview.blade.php
@@ -18,13 +18,55 @@
     position: absolute;
     bottom: 0;
 }
+.invoice-preview .page-wrapper .page .block {
+    padding: 0.6cm 1.2cm;
+}
+.invoice-preview .page-wrapper .page .block.right {
+    left: 50%;
+    width: 50%;
+    position: relative;
+}
 </style>
 
 <div>Invoice preview</div>
 <div class="invoice-preview">
     <div class="page-wrapper">
         <div class="page">
-            {{ $invoice->subject }}
+            <div class="block">
+                <strong>{{ $invoice->account->name }}</strong><br>
+                {{ $invoice->account->street }} {{ $invoice->account->building_number }}<br>
+                {{ $invoice->account->postal_code }} {{ $invoice->account->city }}<br>
+                @if($invoice->account->country != 'CH')
+                    {{ $invoice->account->country }}<br>
+                @endif
+                @if($invoice->account->email)
+                    {{ $invoice->account->email }}<br>
+                @endif
+                @if($invoice->account->phone != 'CH')
+                    {{ $invoice->account->phone }}<br>
+                @endif
+            </div>
+            <div class="block right">
+                @if ($invoice->customer)
+                    <strong>{{ $invoice->customer->name }}</strong><br>
+                    {{ $invoice->customer->street }} {{ $invoice->customer->building_number }}<br>
+                    {{ $invoice->customer->postal_code }} {{ $invoice->customer->city }}<br>
+                    @if($invoice->account->country != 'CH')
+                        {{ $invoice->account->country }}<br>
+                    @endif
+                @endif
+            </div>
+            <div class="block">
+                <h3>Facture</h3>
+                <h5>{{ $invoice->subject }}</h5>
+                <div>
+                    Date: {{ $invoice->date->format('d.m.Y') }}
+                </div>
+            </div>
+            <h5 class="block" style="text-align: right; font-weight: 700">
+                <span style="padding-right: 2cm">Total</span>
+                {{ $invoice->formattedAmount }}
+            </h5>
             <div class="qr-bill">
                 {!! $invoice->generateQrBill() !!}
             </div>
