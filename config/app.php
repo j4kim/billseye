1a0dbcad10694f524108e1002d286c7c4cf7c7c4
Joaquim Perez <jivkim@gmail.com>
2025-05-13 15:11:54 +0200

    add mix_url to deploy orchid on subdirectory

--- config/app.php
+++ config/app.php
@@ -54,6 +54,8 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'mix_url' => env('APP_URL', 'http://localhost'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
